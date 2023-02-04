31728e282eb13046ef50066e5941d99e2173a96f
Dan Bartram <me@danielbartram.com>
2023-02-04 11:07:35 +0000

    Add NamedId functionality
    Create new classes: `NamedId` and `NamedIdForTable` to handle specific IDs. This allows you to give helpful names to specific IDs for easy access later.
    
    e.g. `new NamedId('userWithNoOrders')` is used to define the new named ID, and it can be used elsewhere with `NamedIdForTable('user', 'userWithNoOrders')`.

--- src/utils/recipe-manager.spec.ts
+++ src/utils/recipe-manager.spec.ts
@@ -1,10 +1,10 @@
-import { AutoInc, RecipeManager } from './recipe-manager'
+import { AutoIncId, RecipeManager, NamedId, NamedIdForTable } from './recipe-manager'
 
 describe('prepareRecipes', () => {
   it('generates AutoInc values correctly', () => {
-    const userId = new AutoInc() // 1
-    const productId1 = new AutoInc() // 1
-    const productId2 = new AutoInc() // 2
+    const userId = new AutoIncId() // 1
+    const productId1 = new AutoIncId() // 1
+    const productId2 = new AutoIncId() // 2
 
     const recipeBlock = {
       user: [
@@ -37,5 +37,106 @@ describe('prepareRecipes', () => {
     const manager = new RecipeManager()
     const generatorThing = manager.prepareRecipes([recipeBlock])
     expect(generatorThing.next().value).toEqual(desiredResult)
+  })
+
+  it('generates named IDs correctly', () => {
+    const specialUserId = new NamedId('specialUser')
+    const anotherSpecialUserId = new NamedId('someOtherName')
+
+    const recipeBlock = {
+      user: [
+        { id: new AutoIncId(), email: 'hi@there.com', firstName: 'Eric' },
+        { id: specialUserId, email: 'alice@test.com', firstName: 'Alice' },
+        { id: new AutoIncId(), email: 'bob@example.com', firstName: 'Bob' },
+        { id: anotherSpecialUserId, email: 'another@one.com', firstName: 'Another' },
+      ],
+      orders: [
+        { userId: specialUserId, productId: new NamedId('myProduct'), amount: 5 },
+        { userId: specialUserId, productId: 2, amount: 3 },
+      ],
+    }
+
+    const desiredResult = {
+      user: [
+        { id: 1, email: 'hi@there.com', firstName: 'Eric' },
+        { id: 100000, email: 'alice@test.com', firstName: 'Alice' },
+        { id: 2, email: 'bob@example.com', firstName: 'Bob' },
+        { id: 100001, email: 'another@one.com', firstName: 'Another' },
+      ],
+      orders: [
+        // The name IDs should be incremented on a per-table basis,
+        // both the first named user ID and first named product ID will match here
+        { userId: 100000, productId: 100000, amount: 5 },
+        { userId: 100000, productId: 2, amount: 3 },
+      ],
+    }
+
+    const manager = new RecipeManager()
+    const generatorThing = manager.prepareRecipes([recipeBlock])
+    expect(generatorThing.next().value).toEqual(desiredResult)
+  })
+
+  it('generates the same named ID value for duplicate NamedId instances', () => {
+    const specialUserId = new NamedId('specialUser')
+
+    const recipeBlock = {
+      user: [
+        { id: new AutoIncId(), email: 'hi@there.com', firstName: 'Eric' },
+        { id: specialUserId, email: 'alice@test.com', firstName: 'Alice' },
+        { id: new AutoIncId(), email: 'bob@example.com', firstName: 'Bob' },
+      ],
+      orders: [
+        { userId: new NamedId('specialUser'), productId: 1, amount: 5 },
+        { userId: new NamedId('specialUser'), productId: 2, amount: 3 },
+      ],
+    }
+
+    const desiredResult = {
+      user: [
+        { id: 1, email: 'hi@there.com', firstName: 'Eric' },
+        { id: 100000, email: 'alice@test.com', firstName: 'Alice' },
+        { id: 2, email: 'bob@example.com', firstName: 'Bob' },
+      ],
+      orders: [
+        { userId: 100000, productId: 1, amount: 5 },
+        { userId: 100000, productId: 2, amount: 3 },
+      ],
+    }
+
+    const manager = new RecipeManager()
+    const generatorThing = manager.prepareRecipes([recipeBlock])
+    expect(generatorThing.next().value).toEqual(desiredResult)
+  })
+
+  it('exports generated named IDs correctly', () => {
+    const recipeBlock = {
+      user: [
+        { id: new AutoIncId(), email: 'hi@there.com', firstName: 'Eric' },
+        { id: new NamedId('someUser'), email: 'alice@test.com', firstName: 'Alice' },
+        { id: new AutoIncId(), email: 'bob@example.com', firstName: 'Bob' },
+        { id: new NamedId('someOtherUser'), email: 'another@one.com', firstName: 'Another' },
+      ],
+      orders: [
+        { userId: new NamedIdForTable('user', 'someUser'), productId: new NamedId('myProduct'), amount: 5 },
+        { userId: new NamedIdForTable('user', 'someUser'), productId: new AutoIncId(), amount: 3 },
+      ],
+    }
+
+    const expectedIdMap = {
+      user: {
+        someUser: { id: 100000 },
+        someOtherUser: { id: 100001 },
+      },
+      orders: {
+        myProduct: { id: 100000 },
+      },
+    }
+
+    const manager = new RecipeManager()
+    const generatorThing = manager.prepareRecipes([recipeBlock])
+    // We're not checking the prepared result here, just that the IDs are handled correctly
+    generatorThing.next()
+
+    expect(manager.getGeneratedNamedIds()).toEqual(expectedIdMap)
   })
 })
--- src/utils/recipe-manager.ts
+++ src/utils/recipe-manager.ts
@@ -1,22 +1,81 @@
 
-export type RecipeBundle = Record<string, Array<Record<string, any>>>
+export type RecipeBundle = Record<TableName, Array<Record<ColumnName, any>>>
 
-export class AutoInc {
-  generatedValue: number | null
+export class AutoIncId {
+  value: number | undefined
 }
 
-export class RecipeManager {
-  tableAutoIncIds: Record<string, number> = {}
+export class NamedId {
+  value: number | undefined
+  name: string
 
-  generateAutoIncForTable (tableName: string): number {
+  constructor (name: string) {
+    this.value = undefined
+    this.name = name
+  }
+}
+
+export class NamedIdForTable extends NamedId {
+  tableName: TableName
+
+  constructor (tableName: TableName, name: string) {
+    super(name)
+    this.tableName = tableName
+  }
+}
+
+export type TableName = string
+export type ColumnName = string
+// Allow for description etc to be included in the future
+export type NamedIdExport = {
+  id: number
+}
+export type NamedIdMap = Record<TableName, Record<string, NamedIdExport>>
+
+export class RecipeManager {
+  #tableAutoIncIds: Record<TableName, number> = {}
+  #tableNamedIds: Record<TableName, Set<string>> = {}
+
+  #namedIdRangeStart = 100000
+
+  #generateAutoIncForTable (tableName: string): number {
     let nextAutoIncId = 1
 
-    if (this.tableAutoIncIds[tableName] !== undefined) {
-      nextAutoIncId = this.tableAutoIncIds[tableName] + 1
+    if (this.#tableAutoIncIds[tableName] !== undefined) {
+      nextAutoIncId = this.#tableAutoIncIds[tableName] + 1
     }
 
-    this.tableAutoIncIds[tableName] = nextAutoIncId
+    this.#tableAutoIncIds[tableName] = nextAutoIncId
     return nextAutoIncId
+  }
+
+  #generateNamedIdForTable (tableName: string, idName: string): number {
+    if (!(tableName in this.#tableNamedIds)) {
+      this.#tableNamedIds[tableName] = new Set()
+    }
+
+    this.#tableNamedIds[tableName].add(idName)
+
+    // This allows duplicate named IDs to be defined/reused
+    const indexOfIdName = Array.from(this.#tableNamedIds[tableName]).findIndex(existingName => existingName === idName)
+
+    return this.#namedIdRangeStart + indexOfIdName
+  }
+
+  getGeneratedNamedIds (): NamedIdMap {
+    const result: NamedIdMap = {}
+
+    for (const tableName of Object.keys(this.#tableNamedIds)) {
+      result[tableName] = {}
+
+      for (const namedId of this.#tableNamedIds[tableName]) {
+        result[tableName][namedId] = {
+          id: this.#generateNamedIdForTable(tableName, namedId),
+        }
+      }
+    }
+
+    return result
   }
 
   * prepareRecipes (recipeBundles: RecipeBundle[]): Generator<RecipeBundle> {
@@ -30,15 +89,38 @@ export class RecipeManager {
           const columnNames = Object.keys(tableRow)
 
           for (const columnName of columnNames) {
-            if (tableRow[columnName] instanceof AutoInc) {
+            if (tableRow[columnName] instanceof AutoIncId) {
+              const autoThing: AutoIncId = tableRow[columnName]
               // As they're objects, we can set all uses of this AutoInc instance to have the
               // same generated value
-              if (tableRow[columnName].generatedValue == null) {
-                tableRow[columnName].generatedValue = this.generateAutoIncForTable(tableName)
+              if (autoThing.value === undefined) {
+                autoThing.value = this.#generateAutoIncForTable(tableName)
               }
 
               // Swap out the AutoInc placeholder with its generated value
-              tableRow[columnName] = tableRow[columnName].generatedValue
+              tableRow[columnName] = autoThing.value
+            }
+
+            if (tableRow[columnName] instanceof NamedIdForTable) {
+              const namedIdForTable: NamedIdForTable = tableRow[columnName]
+              // As they're objects, we can set all uses of this AutoInc instance to have the
+              // same generated value
+              if (namedIdForTable.value === undefined) {
+                namedIdForTable.value = this.#generateNamedIdForTable(namedIdForTable.tableName, namedIdForTable.name)
+              }
+
+              // Swap out the NamedId placeholder with its generated value
+              recipeBundle[namedIdForTable.tableName][columnName] = namedIdForTable.value
+            } else if (tableRow[columnName] instanceof NamedId) {
+              const namedId: NamedId = tableRow[columnName]
+              // As they're objects, we can set all uses of this AutoInc instance to have the
+              // same generated value
+              if (namedId.value === undefined) {
+                namedId.value = this.#generateNamedIdForTable(tableName, namedId.name)
+              }
+
+              // Swap out the NamedId placeholder with its generated value
+              tableRow[columnName] = namedId.value
             }
           }
         }
@@ -47,6 +129,6 @@ export class RecipeManager {
       yield recipeBundle
     }
 
-    return Object.keys(this.tableAutoIncIds)
+    return Object.keys(this.#tableAutoIncIds)
   }
 }

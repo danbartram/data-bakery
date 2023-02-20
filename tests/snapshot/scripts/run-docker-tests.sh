5af667d7076d37b370455e16f2ce24e88558d262
Dan Bartram <me@danielbartram.com>
2023-02-20 11:20:23 +0000

    Run the snapshot tests on multiple node versions

--- tests/snapshot/scripts/run-docker-tests.sh
+++ tests/snapshot/scripts/run-docker-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+NODE_VERSION="$1"
 BASE_DIR=$(pwd)
 
-docker run -v "${BASE_DIR}":/code --entrypoint /code/tests/snapshot/scripts/docker-entrypoint.sh node:18-buster-slim
+docker run -v "${BASE_DIR}":/code --entrypoint /code/tests/snapshot/scripts/docker-entrypoint.sh "node:${NODE_VERSION}-buster-slim"

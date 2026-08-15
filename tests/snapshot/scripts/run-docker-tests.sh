7d6f9591e1e739b130c91c73e6c891f3a36537fd
Dan Bartram <me@danielbartram.com>
2026-08-15 12:16:28 +0100

    Upgrade minimum Node version requirement to 18

--- tests/snapshot/scripts/run-docker-tests.sh
+++ tests/snapshot/scripts/run-docker-tests.sh
@@ -2,4 +2,12 @@
 NODE_VERSION="$1"
 BASE_DIR=$(pwd)
 
-docker run -v "${BASE_DIR}":/code --entrypoint /code/tests/snapshot/scripts/docker-entrypoint.sh "node:${NODE_VERSION}-buster-slim"
+declare -a VERSION_IMAGE_MAP
+VERSION_IMAGE_MAP["18"]="node:18-buster-slim"
+VERSION_IMAGE_MAP["20"]="node:20-buster-slim"
+VERSION_IMAGE_MAP["22"]="node:22-bookworm-slim"
+VERSION_IMAGE_MAP["24"]="node:24-trixie-slim"
+
+DOCKER_IMAGE_NAME="${VERSION_IMAGE_MAP[${NODE_VERSION}]}"
+
+docker run -v "${BASE_DIR}":/code --entrypoint /code/tests/snapshot/scripts/docker-entrypoint.sh "${DOCKER_IMAGE_NAME}"
